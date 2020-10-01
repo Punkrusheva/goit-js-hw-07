46dfe6f4ad35d85750361f9ae69d363c872062a4
Pankrusheva Anastasiia <a.pankrusheva@gmail.com>
2020-10-02 00:05:09 +0300

    task-4, 5 done

--- js/task-4.js
+++ js/task-4.js
@@ -1,14 +1,39 @@
-/*Счетчик состоит из спана и кнопок, которые должны увеличивать и уменьшать значение
+/*Счетчик состоит из спана и кнопок, которые должны увеличивать и
+ уменьшать значение
  счетчика на 1.
 
-Создай переменную counterValue в которой будет хранится текущее значение счетчика.
-Создай функции increment и decrement для увеличения и уменьшения значения счетчика
+Создай переменную counterValue в которой будет хранится текущее значение
+ счетчика.
+Создай функции increment и decrement для увеличения и уменьшения значения
+ счетчика
 Добавь слушатели кликов на кнопки, вызовы функций и обновление интерфейса
 <div id="counter">
   <button type="button" data-action="decrement">-1</button>
   <span id="value">0</span>
   <button type="button" data-action="increment">+1</button>
 </div>*/
-const counterValue = value;
-const increment = function (value =+ 1);
-const decrement = function (value =- 1);
+const counter = {
+  value: 0,
+  increment() {
+    this.value += 1;
+  },
+  decrement() {
+    this.value -= 1;
+  },
+};
+const buttonArray = document.querySelectorAll("#counter > button");
+const decrementBtn = buttonArray[0];
+const incrementBtn = buttonArray[1];
+const counterValue = document.querySelector("#value");
+
+decrementBtn.addEventListener("click", () => {
+  counter.decrement();
+  console.log(counter);
+  counterValue.textContent = counter.value;
+});
+
+incrementBtn.addEventListener("click", () => {
+  counter.increment();
+  console.log(counter);
+  counterValue.textContent = counter.value;
+});
--- js/task-5.js
+++ js/task-5.js
@@ -1,4 +1,15 @@
-/*Напиши скрипт который, при наборе текста в инпуте input#name-input (событие input), подставляет его текущее значение в span#name-output. Если инпут пустой, в спане должна отображаться строка 'незнакомец'.
+/*Напиши скрипт который, при наборе текста в инпуте input#name-input 
+(событие input), подставляет его текущее значение в span#name-output.
+ Если инпут пустой, в спане должна отображаться строка 'незнакомец'.
 
 <input type="text" placeholder="Ваше имя?" id="name-input" />
 <h1>Привет, <span id="name-output">незнакомец</span>!</h1>*/
+const refs = {
+  input: document.querySelector("input#name-input"),
+  nameLabel: document.querySelector("span#name-output"),
+};
+refs.input.addEventListener("input", onInputChange);
+function onInputChange(event) {
+  console.log(event.currentTarget.value);
+  refs.nameLabel.textContent = event.currentTarget.value;
+}
